1104fc5d6bb01bed9bca59173b0ff83f1888f058
Zeinep-Balkenova <kkuku3283@gmail.com>
2026-01-26 23:12:08 +0500

    Create syntax.py

---
+++ my-folder/syntax.py
@@ -0,0 +1,16 @@
+if 5 > 2:
+  print("Five is greater than two!")
+
+if 6 = 6:
+  print("EQUAL")
+  
+elif 6 > 4:
+  print("6 greater than 4")
+  
+else:
+  print("no")
+
+fruits = ["apple", "banana", "cherry"]
+for i in fruits:
+    print(i)
+
