cda0c5a0ce5794541ee020c9eb90735fcfcdebd9
Zeinep-Balkenova <kkuku3283@gmail.com>
2026-01-27 01:21:11 +0500

    Create numbers.py

---
+++ my-folder/numbers.py
@@ -0,0 +1,37 @@
+#Example
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+print(type(x))
+print(type(y))
+print(type(z))
+
+#Example
+#Integers:
+q = 1
+w = 35656222554887711
+e = -3255522
+
+print(type(q))
+print(type(w))
+print(type(e))
+
+#Example
+#Floats:
+r = 1.10
+t = 1.0
+u = -35.59
+
+print(type(r))
+print(type(t))
+print(type(u))
+
+#Example
+#complex:
+l = 3+5j
+k = 5j
+j = -5j
+
+print(type(j))
+print(type(k))
+print(type(l))
