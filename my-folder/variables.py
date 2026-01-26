e458a1fae6a61bcb92e4e6a48c659da4c1b7b6f3
Zeinep-Balkenova <kkuku3283@gmail.com>
2026-01-26 23:22:43 +0500

    Create variables.py

---
+++ my-folder/variables.py
@@ -0,0 +1,25 @@
+x = 5
+y = "John"
+print(x)
+print(y)
+
+b = 4       # b is of type int
+b = "Sally" # b is now of type str
+print(b)
+
+v = str(3)    # v will be '3'
+c = int(3)    # c will be 3
+z = float(3)  # z will be 3.0
+
+r = 5
+t = "John"
+print(type(r)) #You can get the data type of a variable with the type() function.
+print(type(t))
+
+q = "John"
+# is the same as
+q = 'John'
+
+a = 4
+A = "Sally"
+#A will not overwrite a
