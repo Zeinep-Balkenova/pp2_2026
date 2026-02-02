ed3e52c09088048a42c8f8f1e60ade4eab6960d8
Zeinep-Balkenova <kkuku3283@gmail.com>
2026-02-02 05:15:27 +0500

    Add if statements for various conditions

---
+++ practice-2/if_else /if_statement.py
@@ -0,0 +1,20 @@
+
+number = 10
+if number > 0:
+    print("Number is positive")
+
+age = 19
+if age >= 18:
+    print("Adult")
+
+temperature = 30
+if temperature > 25:
+    print("It is hot today")
+
+text = "Hello"
+if text:
+    print("Text is not empty")
+
+password = "secure123"
+if len(password) >= 8:
+    print("Password length is OK")
