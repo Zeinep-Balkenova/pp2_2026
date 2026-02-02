e61fc90c66a1ae10495048ddaed4facd7d27b5d8
Zeinep-Balkenova <kkuku3283@gmail.com>
2026-02-02 05:02:59 +0500

    Fix print statements in boolean_operators.py

---
+++ practice-2/boolean/boolean_operators.py
@@ -0,0 +1,18 @@
+age = 20
+has_id = True
+print(age >= 18 and has_id)
+
+temperature = 25
+is_sunny = True
+print(temperature > 20 and is_sunny)
+
+is_weekend = False
+is_holiday = True
+print(is_weekend or is_holiday) 
+
+score = 90
+bonus = False
+print(score > 85 or bonus)
+
+is_raining = False
+print(not is_raining)  
