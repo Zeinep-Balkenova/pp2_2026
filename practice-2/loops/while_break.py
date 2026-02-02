45522171f43e8e22d9263f00928e2e0d7163cac6
Zeinep-Balkenova <kkuku3283@gmail.com>
2026-02-02 05:24:37 +0500

    Add while loops with break conditions
    Implement multiple while loops with break statements.

---
+++ practice-2/loops/while_break.py
@@ -0,0 +1,32 @@
+i = 1
+while True:
+    print(i)
+    if i == 3:
+        break
+    i += 1
+
+number = 0
+while number < 10:
+    if number == 5:
+        break
+    print(number)
+    number += 1
+
+attempts = 0
+while attempts < 5:
+    attempts += 1
+    if attempts == 3:
+        print("Access granted")
+        break
+
+i = 1
+while i <= 10:
+    if i == 7:
+        print("Found 7")
+        break
+    i += 1
+
+while True:
+    print("Loop runs once")
+    break
+
