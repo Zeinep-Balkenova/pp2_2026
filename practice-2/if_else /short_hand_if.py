c600dd3e799a0657e8421617eb75568027f966d2
Zeinep-Balkenova <kkuku3283@gmail.com>
2026-02-02 05:18:43 +0500

    Add shorthand if-else examples in short_hand_if.py

---
+++ practice-2/if_else /short_hand_if.py
@@ -0,0 +1,22 @@
+age = 22
+print("Adult" if age >= 18 else "Minor")
+
+number = 4
+result = "Even" if number % 2 == 0 else "Odd"
+print(result)
+
+score = 67
+status = "Passed" if score >= 50 else "Failed"
+print(status)
+
+is_logged_in = False
+message = "Welcome" if is_logged_in else "Please log in"
+print(message)
+
+a = 5
+b = 8
+print("a is bigger" if a > b else "b is bigger")
+
+
+
+
