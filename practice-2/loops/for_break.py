7528bf6fa93d141c960edc450a8f64ae97063b4a
Zeinep-Balkenova <kkuku3283@gmail.com>
2026-02-02 05:28:44 +0500

    Clean up for_break.py by removing examples
    Removed examples and comments from for_break.py.

---
+++ practice-2/loops/for_break.py
@@ -0,0 +1,26 @@
+for i in range(1, 6):
+    if i == 3:
+        break
+    print(i)
+
+numbers = [1, 4, 7, 9]
+for n in numbers:
+    if n == 7:
+        print("Found 7")
+        break
+
+for char in "python":
+    if char == "h":
+        break
+    print(char)
+
+for i in range(10):
+    if i > 5:
+        break
+    print(i)
+
+words = ["hi", "stop", "go"]
+for word in words:
+    if word == "stop":
+        break
+    print(word)
