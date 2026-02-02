5e9d7836e8ed886b95477cdc0a43229401f9f50d
Zeinep-Balkenova <kkuku3283@gmail.com>
2026-02-02 05:10:47 +0500

    Update if_statement.py

--- practice-2/if_else / /if_statement.py
+++ practice-2/if_else / /if_statement.py
@@ -1,1 +1,33 @@
-/
+number = 1
+if number > 0:
+    print("Number is positive")
+
+age = 1322
+if age >= 18:
+    print("Adult")
+
+temperature = 26
+if temperature > 25:
+    print("It is hot today")
+
+text = "Hello"
+if text:
+    print("Text is not empty")
+
+password = "secure123"
+if len(password) >= 8:
+    print("Password length is OK")
+
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+
+
+
+
+
+
+
+
+
