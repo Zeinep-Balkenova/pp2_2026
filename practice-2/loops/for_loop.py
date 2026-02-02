ee69f7f0c1dc8998292c9cd276d8636b7f3b30e3
Zeinep-Balkenova <kkuku3283@gmail.com>
2026-02-02 05:27:59 +0500

    Add for loop examples in for_loop.py

---
+++ practice-2/loops/for_loop.py
@@ -0,0 +1,17 @@
+for i in range(5):
+    print(i)
+
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+
+total = 0
+for i in range(1, 4):
+    total += i
+print("Sum:", total)
+
+for char in "cat":
+    print(char)
+
+for _ in range(3):
+    print("Hello")
