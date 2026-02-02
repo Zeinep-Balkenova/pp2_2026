5d71d5f1c4385844336f1f3d05d01fd7440f4167
Zeinep-Balkenova <kkuku3283@gmail.com>
2026-02-02 05:22:57 +0500

    Update while_loop.py

--- practice-2/loops/while_loop.py
+++ practice-2/loops/while_loop.py
@@ -1,1 +1,28 @@
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+
+count = 5
+while count > 0:
+    print(count)
+    count -= 1
+
+i = 1
+total = 0
+while i <= 3:
+    total += i
+    i += 1
+print("Sum:", total)
+
+x = 10
+while x > 5:
+    print(x)
+    x -= 1
+
+n = 0
+while n < 3:
+    print("Hello!")
+    n += 1
+
 
