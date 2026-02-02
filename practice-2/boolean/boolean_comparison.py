5da6508a99d2979e01e112bfc18b3051224f02a6
Zeinep-Balkenova <kkuku3283@gmail.com>
2026-02-02 05:01:30 +0500

    Add boolean comparison examples in boolean_comparison.py

---
+++ practice-2/boolean/boolean_comparison.py
@@ -0,0 +1,17 @@
+a = 10
+b = 5
+print(a > b)
+
+x = 7
+y = 7
+print(x == y)
+
+age = 18
+print(age != 21)
+
+name1 = "Anna"
+name2 = "Bella"
+print(name1 < name2) 
+
+word = "hello"
+print(len(word) >= 5)
