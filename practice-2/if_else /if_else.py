5a1e528c083fd54f1bdf1eeb4e6629e826cd8b08
Zeinep-Balkenova <kkuku3283@gmail.com>
2026-02-02 05:13:05 +0500

    Refactor if-else examples and remove comments

---
+++ practice-2/if_else /if_else.py
@@ -0,0 +1,37 @@
+number = -3
+if number >= 0:
+    print("Non-negative number")
+else:
+    print("Negative number")
+
+age = 15
+if age >= 18:
+    print("Adult")
+else:
+    print("Minor")
+
+score = 45
+if score >= 50:
+    print("Passed")
+else:
+    print("Failed")
+
+has_ticket = False
+if has_ticket:
+    print("Access granted")
+else:
+    print("Access denied")
+
+number = 7
+if number % 2 == 0:
+    print("Even number")
+else:
+    print("Odd number")
+
+
+
+
+
+
+
+
