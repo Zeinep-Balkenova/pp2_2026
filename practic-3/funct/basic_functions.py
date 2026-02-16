396969822aef1512d9437deebfb59bd989bbd428
Zeinep-Balkenova <kkuku3283@gmail.com>
2026-02-17 04:31:18 +0500

    Add basic functions for greeting and arithmetic

---
+++ practic-3/funct/basic_functions.py
@@ -0,0 +1,19 @@
+def hello():
+    print("Hello!")
+hello()
+
+def greet(name):
+    print("Hi,", name)
+greet("Anna")
+
+def square(x):
+    print(x * x)
+square(4)
+
+def info():
+    print("Python functions")
+info()
+
+def add(a, b):
+    print(a + b)
+add(3, 5)
