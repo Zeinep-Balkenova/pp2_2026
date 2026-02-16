397675e30fabce25d9c8400bd77f8f89b233ca70
Zeinep-Balkenova <kkuku3283@gmail.com>
2026-02-17 04:33:10 +0500

    Add function examples for greeting, power, and more

---
+++ practic-3/funct/function_arguments.py
@@ -0,0 +1,20 @@
+def greet(name="Guest"):
+    print("Hello,", name)
+greet()
+
+def power(base, exp):
+    print(base ** exp)
+power(2, 3)
+
+def order(item, price):
+    print(item, price)
+order(price=500, item="Book")
+
+def sum3(a, b, c=0):
+    print(a + b + c)
+sum3(1, 2)
+
+def intro(name, age):
+    print(name, age)
+intro("Kate", 20)
+
