1536c3c1ab5693752f7e43613921243b7c2abbeb
Zeinep-Balkenova <kkuku3283@gmail.com>
2026-02-17 04:52:26 +0500

    Add basic inheritance examples in Python

---
+++ practic-3/inheritance/nheritance_basics.py
@@ -0,0 +1,25 @@
+class Animal:
+    pass
+
+class Dog(Animal):
+    pass
+
+class A:
+    def show(self):
+        print("A")
+
+class B(A):
+    pass
+
+class Parent:
+    age = 40
+
+class Child(Parent):
+    pass
+
+class Vehicle:
+    def move(self):
+        print("Moving")
+
+class Car(Vehicle):
+    pass
