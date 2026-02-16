256267418ce23ac0921943e1fab212953edaa343
Zeinep-Balkenova <kkuku3283@gmail.com>
2026-02-17 04:39:32 +0500

    Add lambda functions with map for various operations

---
+++ practic-3/lambda/lambda_with_map.py
@@ -0,0 +1,14 @@
+nums = [1, 2, 3]
+res = list(map(lambda x: x * 2, nums))
+
+names = ["a", "bb"]
+lengths = list(map(lambda x: len(x), names))
+
+nums = [1, 2, 3]
+squares = list(map(lambda x: x**2, nums))
+
+temps = [0, 10]
+c_to_f = list(map(lambda x: x * 9/5 + 32, temps))
+
+words = ["hi", "bye"]
+upper = list(map(lambda w: w.upper(), words))
