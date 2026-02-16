806d72d6873861770f4fd7631393af1cd995fab5
Zeinep-Balkenova <kkuku3283@gmail.com>
2026-02-17 04:49:47 +0500

    Implement Dog, Math, Counter, Greeter, and Box classes
    Added multiple classes: Dog, Math, Counter, Greeter, and Box with basic methods.

---
+++ practic-3/classes/class_methods.py
@@ -0,0 +1,19 @@
+class Dog:
+    def bark(self):
+        print("Woof")
+
+class Math:
+    def add(self, a, b):
+        return a + b
+
+class Counter:
+    def inc(self):
+        self.count += 1
+
+class Greeter:
+    def greet(self, name):
+        print(name)
+
+class Box:
+    def size(self):
+        return 10
