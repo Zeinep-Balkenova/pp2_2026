3bbe62df337cc397c39d731c37c501698773765b
Zeinep-Balkenova <kkuku3283@gmail.com>
2026-02-17 04:48:49 +0500

    Add classes with initialization methods

---
+++ practic-3/classes/init_method.py
@@ -0,0 +1,20 @@
+class User:
+    def __init__(self, name):
+        self.name = name
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Car:
+    def __init__(self):
+        self.speed = 0
+
+class Book:
+    def __init__(self, title):
+        print(title)
+
+class Student:
+    def __init__(self, age=18):
+        self.age = age
