655a0011b9589ad72b133fabf0e1c1cde0a23d01
Zeinep-Balkenova <kkuku3283@gmail.com>
2026-03-31 11:21:19 +0500

    Clarify password prompt in config.py
    Updated the password prompt comment for clarity.

--- Practice7/config.py
+++ Practice7/config.py
@@ -5,5 +5,8 @@ Server [localhost]:
 Database [postgres]:
 Port [5432]:
 Username [postgres]:
-Пароль пользователя postgres:
+Пароль пользователя postgres: когда заполняю он не виден
+
+psql (18.3)
+Введите "help", чтобы получить справку.
 }
