6a5d89395b3a6224181016e90f2dbfd1bdf04546
Zeinep-Balkenova <kkuku3283@gmail.com>
2026-03-31 11:18:10 +0500

    Add DB_CONFIG for database connection
    Added database connection configuration.

--- Practice7/config.py
+++ Practice7/config.py
@@ -1,1 +1,8 @@
-
+# Данные для подключения (замени на свои)
+DB_CONFIG = {
+    "host": "localhost",
+    "database": "phonebook_db",
+    "user": "postgres",
+    "password": "your_password",
+    "port": 5432
+}
